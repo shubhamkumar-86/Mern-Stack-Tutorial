6287ec5f96ffeb2f97a82058ff146ca5173212a4
Shubham Kumar <shubhamkurtha@gmail.com>
2025-12-19 22:43:41 +0530

    variables & data types

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,82 @@
+// variable ko kaise bnate hai javascript me 
+
+// let keyword me hum value ko change kar sakte hai 
+// let name = "Shubham";
+// let age = 24;
+
+// age =30;
+
+// console.log(name, age);
+
+// const me hum value ko change nhi kar sakte hai assign karne ke bad, const ko declare karte time hi value chahiye hota hai use hum khali nhi chhod sakte ye uska rule hai. 
+
+// const account = 1234;
+// account = 546;
+
+// console.log(account);
+
+// old tarika 
+
+// var sirf functional scope ko respect karta hai or glbal scope ko bhi.
+// ise hum do bar redeclare bhi kar sakte hai 
+
+// var a = 10;
+// var a = 20;
+
+// Data types 
+
+// primitive data type
+// number, string, boolean, undefined, null, bigint, symbol
+
+
+// number
+// let a= 10; 
+// let b=  2.36;
+// console.log(a,b);
+
+// string
+// let a = "shubham";
+// console.log(a);
+
+// boolean
+// let login = true;
+// console.log(login);
+
+// undefined - declare karke koi value nhi dalne par undefined aata hai.
+
+// let user;
+// console.log(user);
+
+// bigint
+// let num = 245789545789556n;
+// console.log(num);
+
+// null
+// let weather =null;
+// console.log(weather);
+
+// symbol - unique value generate ho jati hai isse.
+// const id1 = Symbol("id");
+// console.log(id1);
+
+// non primitive data type
+// array, object, function
+
+// let arr = ["shubham", 25, 24, 66, 45, true];
+// console.log(arr);
+
+// let obj={
+//     name:"shubham",
+//     account:121212,
+//     age:24,
+//     category:"gen"
+// };
+
+// console.log(obj);
+
+// let s = function add(){
+//     console.log("hello");
+// }
+
+// console.log(s);
+// s(); -- function calling
