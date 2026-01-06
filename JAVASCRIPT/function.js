027753bc57ec6f00420b7a4a98693ffaf6c1430a
Shubham Kumar <shubhamkurtha@gmail.com>
2026-01-06 23:04:09 +0530

    function

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -1,2 +1,41 @@
 // Function in Javascript 
 
+// function greeting() {
+//     console.log("hello, welcome to learning function");
+// }
+
+// greeting();
+
+// Rest operator (VVI)
+
+// function addNumber(...num){
+//     let sum=0;
+//     for(let n of num){
+//         sum+=n;
+//     }
+//     console.log(sum);
+// }
+
+// addNumber(47,58,12);
+
+// Spread operator
+
+// const arr =[10,20,40,50,60];
+// const arr2 = [70,80,90];
+
+// const [first,second, ...num] = arr;
+// console.log(first,second, num);
+
+// const ans = [arr,arr2];
+// console.log(ans);
+
+// function: expression
+
+// const addNumber =function(num1,num2){
+//     return num1+num2;
+// }
+
+// console.log(addNumber(3,5));
+
+// arrow function
+
