6275f88c9302fb895851b4a18a75465f0305b8ab
Shubham Kumar <shubhamkurtha@gmail.com>
2026-02-11 23:09:14 +0530

    map

---
+++ JAVASCRIPT/map_filter.js
@@ -0,0 +1,18 @@
+// forEach, MAP, FILTER, REDUCE, SET in JAVASCRIPT
+
+// const arr = [10, 20, 30, 5, 35];
+// let sum = 0;
+
+// arr.forEach((number)=>{
+//     sum+=number;
+// })
+
+// console.log(sum);
+
+// Filter
+
+const arr = [10, 20, 30, 5, 35];
+
+// const newArr = arr.filter((number)=> number>25);
+//  console.log(newArr);
+
